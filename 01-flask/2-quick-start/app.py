1477818e1a045a13038f097a648ce3349021b51f
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-08-04 07:41:52 +0530

    first app.py

--- 01-flask/2-quick-start/app.py
+++ 01-flask/2-quick-start/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)                     # Create the Flask app instance
 def hello_world():                        # Define the view function for '/'
     return 'Hello World'                  # Send this text as the response
 if __name__ == '__main__':                # Run only if this file is executed directly
-   # app.run()                            # Start the Flask development server
+   # app.run()                            # Start the Flask development server (This line is by default)
 
     app.run(debug=True)
         # Starts the Flask development server with debug mode enabled.
