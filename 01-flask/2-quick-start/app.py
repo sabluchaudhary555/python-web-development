95e0eeba58dafda31c95ad40bc444d00225ac8c9
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-08-10 06:59:46 +0530

    first app.py

--- 01-flask/2-quick-start/app.py
+++ 01-flask/2-quick-start/app.py
@@ -1,4 +1,4 @@
-from flask import Flask                  # Import the Flask class
+from flask import Flask                   # Import the Flask class
 app = Flask(__name__)                     # Create the Flask app instance
 @app.route('/')                           # Map URL '/' to the function below
 def hello_world():                        # Define the view function for '/'
@@ -8,4 +8,4 @@ if __name__ == '__main__':                # Run only if this file is executed di
 
     app.run(debug=True)
         # Starts the Flask development server with debug mode enabled.
-        # debug=True auto-reloads the server on code changes and shows detailed error pages in the browser.
+        # debug=True, auto-reloads the server on code changes and shows detailed error pages in the browser.
