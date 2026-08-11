7cbc2e24664ef3199d2b339a43c7cd443e7c9827
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-08-11 06:25:55 +0530

    added varible route

--- 01-flask/2-quick-start/route.py
+++ 01-flask/2-quick-start/route.py
@@ -1,9 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
 @app.route('/hello')
 def hello():
     return "Welcom to Hello World"
+
+@app.route('/user/<username>')
+def user(username):
+    return f'Welcome to {username}'
+
 @app.route('/')
 def index():
     return "Home Page"
