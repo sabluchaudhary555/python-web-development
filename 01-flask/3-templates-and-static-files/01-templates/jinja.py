ed9735b503a15dd3d5b2c87977951b24dcb0d3d9
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-08-22 11:42:34 +0530

    jinja2 rendering of loops

---
+++ 01-flask/3-templates-and-static-files/01-templates/jinja.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome! Visit /yourname to see the dynamic greeting."
+
+@app.route("/<name>")
+def welcome(name):
+    return render_template("welcome.html", name=name)
+
+@app.route("/about")
+def about():
+    sites = ['twitter', 'facebook', 'instagram', 'whatsapp']
+    return render_template("about.html", sites=sites)
+
+if __name__ == "__main__":
+    app.run(debug=True)
